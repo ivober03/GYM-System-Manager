bff8ba5a9505cff666e88dbc7befac83371f86d3
Ivo Bermudez <ivobermudez03@gmail.com>
2024-02-14 23:02:51 -0300

    a

--- static/js/main.js
+++ static/js/main.js
@@ -165,161 +165,6 @@
   }
 
 
-  function updateHabitTimes() {
-    // Get all <p> elements that display the elapsed time
-    var habitTimeElements = document.querySelectorAll('[id^="habit-time-"]');
-
-    // Iterate over each item and update the elapsed time for the corresponding habit
-    habitTimeElements.forEach(function(element) {
-      // Get the habit ID from the <p> element ID
-      var habitId = element.id.split('-')[2];
-
-      // Get the start date of the corresponding habit
-      var startDateString = document.getElementById('habitStartDate-' + habitId).value;
-      var startDate = new Date(startDateString);
-
-      // Get current date
-      var currentDate = new Date();
-
-      // Calculate the difference in milliseconds between the current date and the start date
-      var timeDiff = currentDate - startDate;
-
-      // Calculate the days, hours, minutes and seconds elapsed
-      var days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-
-      // Update the content of the <p> element with the elapsed time
-      element.textContent =  days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds";
-    });
-  }
-
-  // Update the elapsed time every second
-  setInterval(updateHabitTimes, 1000);
-
-  // Get all buttons to show progress good habit
-  var progressModalBtns = document.querySelectorAll('.display-progress-modal');
-
-  // Handle the click event on each button
-  progressModalBtns.forEach(function(progressModalBtn) {
-    progressModalBtn.addEventListener('click', function() {
-      var habitId = this.getAttribute('data-habit-id');
-
-      // Make an AJAX request to get the data
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', '/get_progress_data/' + habitId);
-      xhr.onload = function() {
-        if (xhr.status === 200) {
-          var data = JSON.parse(xhr.responseText);
-          var habit = data.habit_data;
-          var record = data.record_data;
-
-          // Complete modal with the data
-          document.getElementById('habitProgressModalLabel').textContent = habit.name;
-          document.getElementById('currentStreak').textContent = record.current_streak + ' day/s';
-          document.getElementById('completedDays').textContent = habit.completed_count + ' day/s';
-          document.getElementById('failedDays').textContent = habit.failed_count + ' day/s';
-
-          // Generate the URL with the name of the habit
-          var habitName = habit.name
-          var viewRecordsUrl = '/records?query=' + encodeURIComponent(habitName);
-
-          // Assign the URL to the "View Records" button
-          var viewRecordsBtn = document.getElementById('viewRecordsBtn');
-          viewRecordsBtn.setAttribute("href", viewRecordsUrl)
-
-          // Get the progress modal
-          var progressModal = document.getElementById('habitProgressModal');
-
-          // Open the progress modal
-          var modalInstance = new bootstrap.Modal(progressModal);
-          modalInstance.show();
-
-          // Get all buttons to close the modal
-          var closeButtons = progressModal.querySelectorAll('[data-bs-dismiss="modal"]');
-
-          // Handle the click event for each button
-          closeButtons.forEach(function(closeButton) {
-            closeButton.addEventListener('click', function() {
-              // Close modal
-              modalInstance.hide();
-            });
-          });
-        } else {
-          alert('Este hábito no tiene ningún registro. Una vez que lo completes o falles, los datos aparecerán aquí.');
-        }
-      };
-      xhr.send();
-    });
-  });
-
-
-  // Get all buttons to show progress bad habit
-  var badProgressModalBtns = document.querySelectorAll('.display-progress-modal-bad');
-
-  // Handle the click event on each button
-  badProgressModalBtns.forEach(function(badProgressModalBtn) {
-    badProgressModalBtn.addEventListener('click', function() {
-      var habitId = this.getAttribute('data-habit-id');
-
-      // Make an AJAX request to get the data
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', '/get_progress_data/' + habitId);
-      xhr.onload = function() {
-        if (xhr.status === 200) {
-          var data = JSON.parse(xhr.responseText);
-          var habit = data.habit_data;
-          var record = data.record_data;
-
-          // Get habit start date
-          var startDate = new Date(habit.start_date);
-
-          // Get current date
-          var currentDate = new Date();
-
-          // Calculate the difference in milliseconds between the current date and the start date
-          var timeDiff = currentDate - startDate;
-
-          // Calculate the days, hours, minutes and seconds elapsed
-          var days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-          var hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-          var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-          var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-
-          // Complete modal with the data
-          document.getElementById('badHabitProgressModalLabel').textContent = habit.name;
-          document.getElementById('badCurrentStreak').textContent = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds";
-          document.getElementById('badFailedDays').textContent = habit.failed_count + ' day/s';
-
-          // Generate the URL with the name of the habit
-          var habitName = habit.name
-          var viewRecordsUrl = '/records?query=' + encodeURIComponent(habitName);
-
-          // Assign the URL to the "View Records" button
-          var viewRecordsBtn = document.getElementById('viewRecordsBtnBad');
-          viewRecordsBtn.setAttribute("href", viewRecordsUrl)
-
-          // Open the progress modal
-          var badProgressModal = new bootstrap.Modal(document.getElementById('badHabitProgressModal'));
-          badProgressModal.show();
-
-          var closeButtons = document.querySelectorAll('[data-bs-dismiss="modal"]');
-          closeButtons.forEach(function(closeButton) {
-            closeButton.addEventListener('click', function() {
-              // Close modal
-              badProgressModal.hide();
-            });
-          });
-
-        } else {
-          alert('Este hábito no tiene ningún registro. Una vez que lo completes o falles, los datos aparecerán aquí.');
-        }
-      };
-      xhr.send();
-    });
-  });
-
 // Get all plan edit buttons
 var editPlanBtns = document.querySelectorAll('.edit-plan-btn');
 
@@ -367,105 +212,56 @@ editPlanBtns.forEach(function(editPlanBtn) {
   });
 });
 
+// Get all member buttons
+var editMemberBtns = document.querySelectorAll('.edit-member-btn');
 
-  // Initialize all Pikaday calendars
-  var picker = new Pikaday({
-    field: document.getElementById('datepicker'),
-    format: 'YYYY-MM-DD',
-    minDate: new Date(),
-    defaultDate: new Date()
-  });
+// Handle click event for each button
+editMemberBtns.forEach(function(editMemberBtn) {
+  editMemberBtn.addEventListener('click', function() {
+    var memberId = this.getAttribute('data-member-id');
 
-  var pickerEdit = new Pikaday({
-    field: document.getElementById('datepickerEdit'),
-    format: 'YYYY-MM-DD',
-    minDate: new Date(),
-    defaultDate: new Date()
-  });
+    // Make an AJAX request to get the data
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/get_member/' + memberId);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        var member = JSON.parse(xhr.responseText);
 
-  var pickerBreak = new Pikaday({
-    field: document.getElementById('datepickerBreak'),
-    format: 'YYYY-MM-DD',
-    minDate: new Date(),
-    defaultDate: new Date()
-  });
+        // Complete modal with the data
+        document.getElementById('editMemberName').value = member.name;
+        document.getElementById('editMemberPlan').value = member.plan_id;
 
-  var pickerBreak2 = new Pikaday({
-    field: document.getElementById('datepickerBreak2'),
-    format: 'YYYY-MM-DD',
-    minDate: new Date(),
-    defaultDate: new Date()
+
+        // Get form reference
+        var form = document.getElementById('editPlanForm');
+
+        // Update the 'action' attribute of the form
+        form.action = '/edit_plan/' + planId;
+
+        // Open edit plan modal
+        var editPlanModal = new bootstrap.Modal(document.getElementById('editPlanModal'));
+        editPlanModal.show();
+
+        var closeButtons = document.querySelectorAll('[data-bs-dismiss="modal"]');
+        closeButtons.forEach(function(closeButton) {
+          closeButton.addEventListener('click', function() {
+            editPlanModal.hide();
+          });
+        });
+
+      } else {
+        alert('Error al obtener los datos del plan');
+      }
+
+    };
+    xhr.send();
   });
+});
 
   var currentDate = moment().format('ddd MMM DD YYYY'); // Get formated current date
   console.log(currentDate);
   document.getElementById('datepicker').setAttribute('placeholder', currentDate);
   document.getElementById('datepickerBreak').setAttribute('placeholder', currentDate);
   document.getElementById('datepickerBreak2').setAttribute('placeholder', currentDate);
-
-  document.addEventListener("DOMContentLoaded", function() {
-    // Get references to the "Remove" and "Limit" buttons
-    var quitButton = document.querySelector("[data-habit-type='quit']");
-    var limitButton = document.querySelector("[data-habit-type='limit']");
-
-    // Get references to the "quitSection" and "limitSection" sections
-    var quitSection = document.getElementById("quitSection");
-    var limitSection = document.getElementById("limitSection");
-
-    // Show "quitSection" and hide "limitSection" by default
-    quitSection.classList.add("d-block");
-    limitSection.classList.add("d-none");
-    document.getElementById('habitTypeInput').value = 'quit';
-    console.log("Valor de habitTypeInput:", document.getElementById("habitTypeInput").value);
-
-    // Set button styles
-    quitButton.classList.add("btn-light");
-    quitButton.classList.add("active");
-    limitButton.classList.add("btn-light");
-
-    // Function to handle the "Quit" button click event
-    quitButton.addEventListener("click", function() {
-      // Show "quitSection" and hide "limitSection" when pressing the "Quit" button
-      quitSection.classList.add("d-block");
-      quitSection.classList.remove("d-none");
-      limitSection.classList.add("d-none");
-      limitSection.classList.remove("d-flex");
-
-      // Assign 'quit' value to habitTypeInput
-      document.getElementById('habitTypeInput').value = 'quit';
-      console.log("Valor de habitTypeInput:", document.getElementById("habitTypeInput").value);
-    });
-
-    // Function to handle the "Limit" button click event
-    limitButton.addEventListener("click", function() {
-      // Show "limitSection" and hide "quitSection" when pressing the "Limit" button
-      limitSection.classList.add("d-flex");
-      limitSection.classList.remove("d-none");
-      quitSection.classList.add("d-none");
-      quitSection.classList.remove("d-block");
-
-      // Assign 'limit' value to habitTypeInput
-
-
-      document.getElementById('habitTypeInput').value = 'limit';
-      console.log("Valor de habitTypeInput:", document.getElementById("habitTypeInput").value);
-    });
-  });
-
-  // Initialize Flatpickr in the time input field
-  flatpickr("#habitShowAt", {
-    enableTime: true,
-    noCalendar: true,
-    dateFormat: "H:i",
-    time_24hr: true
-  });
-
-  // Initialize Flatpickr in the time input field
-  flatpickr("#editHabitShowAt", {
-    enableTime: true,
-    noCalendar: true,
-    dateFormat: "H:i",
-    time_24hr: true
-  });
 
 })();
